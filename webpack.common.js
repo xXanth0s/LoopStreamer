329cf6acaff63f2eba938adde7bb0c5a0104a602
Maximilian Lang <maxi.schore@gmail.com>
2020-05-19 15:26:07 +0200

    add extension

--- webpack.common.js
+++ webpack.common.js
@@ -1,17 +1,23 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const ExtensionReloader = require('webpack-extension-reloader');
+
+const extensionPath = 'extension';
 
 module.exports = {
   entry: {
-    content: [
+    'js/content': [
       './src/content/main.ts',
     ],
+    'extension/background': './extension/background/main.ts',
+    'extension/content': './extension/content/main.ts',
   },
   target: 'electron-preload',
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, 'dist_electron/js'),
+    path: path.resolve(__dirname, 'dist_electron'),
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
@@ -43,5 +49,31 @@ module.exports = {
   },
   plugins: [
     new VueLoaderPlugin(),
+
+    new ExtensionReloader({
+      entries: { // The entries used for the content/background scripts or extension pages
+        contentScript: 'extension/content',
+        background: 'extension/background',
+      },
+      reloadPage: false,
+    }),
+    new CopyPlugin([
+      {
+        from: 'assets',
+        to: 'assets',
+      },
+      {
+        from: 'assets',
+        to: 'extension/assets',
+      },
+      {
+        from: './extension/_locales',
+        to: 'extension/_locales',
+      },
+      {
+        from: './extension/manifest.json',
+        to: 'extension/manifest.json',
+      },
+    ]),
   ],
 };
