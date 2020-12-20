70c1b6b518b72ee0cc1712ec1252f5881186c7f6
xXanth0s <maxi.schore@gmail.com>
2020-12-20 19:07:26 +0100

    adding remote dev tools

--- src/store/store/background-store.ts
+++ src/store/store/background-store.ts
@@ -1,4 +1,4 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { applyMiddleware, configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
 import { forwardToRenderer, replayActionMain, triggerAlias } from 'electron-redux';
 import { StateModel } from '../models/state.model';
@@ -6,6 +6,7 @@ import { StateModel } from '../models/state.model';
 import { getStorageData, StorageMiddlerware } from '../middleware/storage.middlerware';
 import { sagaErrorHandler, watcherSaga } from '../saga/watcher.saga';
 import { storeReducers } from '../reducers/reducer.collection';
+import { composeWithDevTools } from 'remote-redux-devtools';
 
 const preloadedState = getStorageData();
 const sagaMiddleware = createSagaMiddleware({ onError: sagaErrorHandler });
@@ -15,13 +16,13 @@ const middleware = [
     StorageMiddlerware,
     forwardToRenderer,
 ];
+const composeEnhancer = composeWithDevTools({ hostname: 'localhost', port: 8800 })(applyMiddleware(...middleware));
 
 const backgroundStore = configureStore<StateModel>({
     reducer: storeReducers,
     // @ts-ignore
-    middleware,
-    devTools: true,
     preloadedState,
+    enhancers: [ composeEnhancer ]
 });
 
 console.log('background store loaded ');
