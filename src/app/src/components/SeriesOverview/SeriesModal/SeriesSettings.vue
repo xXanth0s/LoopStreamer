f877b8c6dad30e411e3c91ab61a5d6d6052b6575
xXanth0s <maxi.schore@gmail.com>
2021-01-05 13:45:09 +0100

    create series settings component and add slider dependency

---
+++ src/app/src/components/SeriesOverview/SeriesModal/SeriesSettings.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+
+    </div>
+
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+    import Component from 'vue-class-component';
+    import { Prop } from 'vue-property-decorator';
+    import { Series } from '../../../../../store/models/series.model';
+
+    @Component({
+        name: 'series-settings',
+    })
+    export default class SeriesSettings extends Vue {
+        @Prop(Object)
+        private series: Series;
+    }
+</script>
+
+<style scoped>
+
+</style>
